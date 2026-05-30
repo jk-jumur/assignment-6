8f46ffed9286c0395ff5e544b346177f6f44e450
jk-jumur <developer.jumur@gmail.com>
2026-05-30 11:07:00 +0600

    DigiTools-Platform  project setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import NavBar from './Components/NavBar'
+
+function App() {
+ 
+
+  return (
+    <>
+       <h2 className='text-3xl bg-red-400 text-white'>Hello Google</h2>
+     <NavBar/>
+    
+     
+
+   
+    </>
+  )
+}
+
+export default App
